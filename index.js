04f0d06ef22e4f208f9e1c54dd0142007d308777
crimera <crimera@users.noreply.github.com>
2025-11-17 12:00:00 -0300

    Update every file header to ISO date & update year in copyright notice

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+ * File: index.js
+ * Project: mass-comment-bot
+ * Created Date: 2025-08-11 12:10:00
+ * Author: criimera
+ *
+ * Last Modified: 2025-01-01 18:10:12
+ * Modified By: criimera
+ *
+ * Copyright (c) 2025 - 2026 criimera <https://github.com/criimera>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+// Abfahrt!
+require("./src/bot.js").run();
